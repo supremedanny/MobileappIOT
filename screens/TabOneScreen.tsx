5b2ab300f6eda2684c17c6ed999ad3dff9092cec
Kenneth-Aponte <kenneth.aponte2@upr.edu>
2021-04-03 09:47:16 -0400

    Implemented QR Code from users ID.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
-import {Pressable, StyleSheet} from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import {Pressable, SafeAreaView, StyleSheet} from 'react-native';
+import QRCode from 'react-native-qrcode-generator';
+import * as firebase from "firebase";
 import { Text, View } from '../components/Themed';
+import {useRef, useState} from "react";
 
 export default function TabOneScreen() {
+  const user = firebase.auth().currentUser;
+  const userID = user?.uid;
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>QR CODE</Text>
-    </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.qrCodeContainer}>
+          <Text style={styles.title}>Scan the barcode below to collect points.</Text>
+
+          <QRCode
+              //QR code value
+              value={(userID != undefined) ? userID : 'invalid'}
+              {...(userID == undefined) ? alert("Invalid QR Code. Contact support."): undefined}
+              //sends alert if user has no UID ("This should never happen.")
+              //size of QR Code
+              size={350}
+              bgColor={'black'}
+              fgColor={'white'}
+          />
+
+        </View>
+      </SafeAreaView>
   );
 }
 
@@ -17,12 +34,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black',
+    backgroundColor: '#E7FBEB',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#fff'
+    alignContent: 'center',
+    color: 'black',
+    backgroundColor: '#E7FBEB'
+  },
+  qrCodeContainer:{
+    backgroundColor: 'white',
+    alignItems: 'center',
   },
   separator: {
     marginVertical: 30,
